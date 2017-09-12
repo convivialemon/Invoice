1ec79e1dc6bf3394e40252f5e385d9fd1da7b901
convivialemon <convivialemon2@gmail.com>
2017-09-12 20:19:51 +0600

    Add files via upload
    First upload

---
+++ NewClass.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+class NewClass {
+    public static void main(String args[]){
+        Scanner emon = new Scanner(System.in);
+        System.out.println(emon.nextLine());      
+    }
+}
